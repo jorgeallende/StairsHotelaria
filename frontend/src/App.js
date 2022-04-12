fc9b0e33814f298d679c24405b6f5e767fd58d6a
Valdir Junior <valdir@solarz.com.br>
2022-04-12 10:32:51 -0300

    fix. React router dom quebrado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import ClientLogin from "./pages/clientLogin/ClientLogin";
 import AdminLogin from "./pages/adminLogin/AdminLogin";
